4768058c0ce6652838955cd2d714b41c1bc3c64d
shivam7703 <shivamg7703@gmail.com>
2026-06-02 18:14:30 +0530

    eighteen

--- component/contact/contact-form.tsx
+++ component/contact/contact-form.tsx
@@ -287,7 +287,7 @@ function ContactForm() {
     setErrorMsg(null);
 
     try {
-      const response = await fetch("/api/contact", {
+      const response = await fetch("http://localhost:5000/api/submit-form", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(fields),
